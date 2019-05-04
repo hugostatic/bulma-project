ec32231a1cc5a4c5539a7195b8c1a972056cd7d2
jean-Marc Kraëber <jmarck4@gmail.com>
2019-05-04 08:32:06 +0200

    nouvelle hero section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+    
+    
+     //Mobile menu toggle
+     if ($('.navbar-burger').length) {
+        $('.navbar-burger').on("click", function () {
+
+            var menu_id = $(this).attr('data-target');
+            $(this).toggleClass('is-active');
+            $("#" + menu_id).toggleClass('is-active');
+            $('.navbar.is-light').toggleClass('is-dark-mobile')
+        });
+    }
+
+    //Navbar Clone
+    if ($('#navbar-clone').length) {
+        $(window).scroll(function () {    // this will work when your window scrolled.
+            var height = $(window).scrollTop();  //getting the scrolling height of window
+            if (height > 50) {
+                $("#navbar-clone").addClass('is-active');
+            } else {
+                $("#navbar-clone").removeClass('is-active');
+            }
+        });
+    }
+
+
+    
+
     // open modal
     $('#bt-modal').click(function() {
         $('#modal-page').css(
